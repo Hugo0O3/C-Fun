7b5f4d7c9540bde15b9979590c1c7f2530d139bf
Hugo0O3 <hugomancini2.0@gmail.com>
2025-03-25 23:18:30 +0100

    pas bon doc .net crash

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -2,16 +2,17 @@ static class LogLine
 {
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+
+        return logLine;
     }
 
     public static string LogLevel(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
+        
     }
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        
     }
 }
